99887f4f7d3f71914795ee9b45a37740ab48c44e
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-02 16:21:53 +0530

    Variable Scope

---
+++ VariableScope.java
@@ -0,0 +1,14 @@
+public class VariableScope {
+
+    static int x = 3; //CLASS
+    public static void main(String[] args){
+        int x =1;  //LOCAL
+        System.out.println(x);
+
+        doSomething();
+    }
+    static void doSomething(){
+        int x =2; //LOCAL
+        System.out.print(x);
+    }
+}
