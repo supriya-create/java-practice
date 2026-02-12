1403c14723f7d654758385c031c6a49eb0d5138a
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-12 20:19:52 +0530

    ArrayList

---
+++ Arraylist.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+public class Arraylist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> foods = new ArrayList<>();
+
+        System.out.print("Enter the number of food you would like: ");
+        int numOffoods = sc.nextInt();
+        sc.nextLine();
+
+        for(int i=1;i<=numOffoods; i++){
+            System.out.print("Enter food number"+i+" :");
+            String food = sc.nextLine();
+            foods.add(food);
+        }
+        System.out.print(foods);
+        sc.close();
+    }
+}
