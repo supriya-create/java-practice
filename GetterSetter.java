981139d75235c089a800691b878fe0c94859949a
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-15 23:30:39 +0530

    Getter Setter

--- GetterSetter.java
+++ GetterSetter.java
@@ -3,7 +3,7 @@ public class GetterSetter {
         Jewel jewel = new Jewel("Ring", "Gold", 30000);
 
         jewel.setMetal("Diamond");  //setter - write/change value
-        jewel.setPrice(50000);
+        jewel.setPrice(60000);
 
         System.out.println(jewel.getMetal()+" "+jewel.getName()+" "+jewel.getPrice()); //getter- print private value 
     }
