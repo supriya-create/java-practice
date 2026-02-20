0fc457710c927b44540a32f013ceca14318d8225
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-20 22:28:52 +0530

    String toLowerCase Method

---
+++ LowercaseNC.java
@@ -0,0 +1,16 @@
+// Meet Jancy, a diligent student learning to master programming. She is working on a project that requires her to process text data. Today, she needs to convert a given string to lowercase to ensure consistent and uniform text.
+
+
+
+// Write a program that takes an input string and converts it to lowercase to help out Jancy. 
+
+import java.util.Scanner;
+
+public class LowercaseNC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.toLowerCase());
+        sc.close();
+    }
+}
