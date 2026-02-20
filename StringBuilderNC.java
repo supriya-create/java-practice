47c715e11144bb2cc160a940c7a70592b5208525
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-20 22:42:19 +0530

    String Builder insertion

---
+++ StringBuilderNC.java
@@ -0,0 +1,20 @@
+// Ashok is developing a text processing utility that allows users to insert a substring into an original string at a specified position. The program takes input for the original string, the substring to insert, and the position at which to insert the substring.
+
+
+
+// Write a program to help Ashok using the methods of the String Builder class.
+
+import java.util.Scanner;
+
+public class StringBuilderNC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String org = sc.nextLine();
+        String sub = sc.nextLine();
+        int pos = sc.nextInt();
+        StringBuilder sb =new StringBuilder(org);
+        sb.insert(pos, sub);
+        System.out.println(sb.toString());
+        sc.close();
+    }
+}
