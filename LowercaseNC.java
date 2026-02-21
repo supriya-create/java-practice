99e5a6485d9e634fef221bb6849cb9a7127d11d3
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-21 22:43:31 +0530

    String Lowercase

--- LowercaseNC.java
+++ LowercaseNC.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class LowercaseNC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input: ");
         String str = sc.nextLine();
         System.out.println(str.toLowerCase());
         sc.close();
