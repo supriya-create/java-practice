0544b73a77108e0e18a1c81b28bfee98c9e8e67b
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-24 23:14:23 +0530

    Swap Two numbers

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,15 @@
+
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        
+        int a = 9;
+        int b = 8;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
