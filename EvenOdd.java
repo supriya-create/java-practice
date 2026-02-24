eb0e947e891399efde8d69d51e8c7db915c4cb3e
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-24 23:16:59 +0530

    Even odd using ternary opertaor

---
+++ EvenOdd.java
@@ -0,0 +1,7 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+        String result = (num%2==0) ? "EVEN" : "ODD";
+        System.out.println(result);
+    }
+}
--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -1,5 +1,3 @@
-
-
 public class SwapTwoNumbers {
     public static void main(String[] args) {
         
