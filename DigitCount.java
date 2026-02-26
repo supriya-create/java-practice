83f25292460e4489b81143d067acf70c7c95ab05
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-26 20:19:08 +0530

    Count Digit in a number

---
+++ DigitCount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DigitCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the numver: ");
+        int num = sc.nextInt();
+        int count =0;
+        while(num!=0){
+            num = num/10;
+            count++;
+        }
+        System.out.println("Number of digits present in number: "+count);
+        sc.close();
+    }
+}
