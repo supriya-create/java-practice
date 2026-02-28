e63aba9b6170022c32ced87ed7225ba23f22fc6c
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-02-28 20:20:04 +0530

    28 Feb commit

--- VarargsSum.java
+++ VarargsSum.java
@@ -7,6 +7,6 @@ public class VarargsSum {
         return total;
     }
     public static void main(String[] args) {
-        System.out.println(sum(4,2,8,5));
+        System.out.println("Sum is: "+sum(4,2,8,5));
     }
 }
