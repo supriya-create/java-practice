ccbb413f436a62d76faabca817d732d818366840
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-03-01 13:02:55 +0530

    Discount Calculator

---
+++ DiscountCalculatorNC.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DiscountCalculatorNC {
+    int b;
+    int d;
+    {
+        Scanner sc = new Scanner(System.in);
+        b = sc.nextInt();
+        d = sc.nextInt();
+        sc.close();
+    }
+    public double calculateDiscountedPrice(){
+        return b - (b*d/100);
+    }
+    public double calculateAmountSaved(){
+        return b*d/100;
+    }
+    public static void main(String[] args) {
+        DiscountCalculatorNC discountCalculator = new DiscountCalculatorNC();
+        double discountedPrice = discountCalculator.calculateDiscountedPrice();
+        double amountSaved = discountCalculator.calculateAmountSaved();
+
+        System.out.printf("Discounted Price: Rs. %.2f%n", discountedPrice);
+        System.out.printf("Amount Saved: Rs. %.2f%n", amountSaved);
+    }
+}
