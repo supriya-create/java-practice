20dbbcf1f90d5401dec14ca88374acf3c375d231
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-03-06 22:06:58 +0530

    6 March commit

---
+++ HierarchicalInheritance.java
@@ -0,0 +1,27 @@
+class Shape{
+    void display(){
+        System.out.println("This is shape");
+    }
+}
+class Rectangle extends Shape{
+    void rectangle(){
+        System.out.println("This is rectangle");
+    }
+}
+class Circle extends Shape{
+    void circle(){
+        System.out.println("This is circle");
+    }
+}
+public class HierarchicalInheritance {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        Circle c = new Circle();
+
+        r.rectangle();
+        r.display();
+
+        c.circle();
+        c.display();
+    }
+}
