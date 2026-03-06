56c395b9a572ac35bf7ff80406ea246258b1b8c4
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-03-06 21:58:05 +0530

    6 March commit

---
+++ MultilevelInheritance.java
@@ -0,0 +1,23 @@
+class Vehical{
+    void start(){
+        System.out.println("Vehical starts");
+    }
+}
+class Car extends Vehical{
+    void drive(){
+        System.out.println("Car is driving");
+    }
+}
+class ElectricCar extends Car{
+    void charge(){
+        System.out.println("Electric car charging");
+    }
+}
+public class MultilevelInheritance {
+    public static void main(String[] args) {
+        ElectricCar e = new ElectricCar();
+        e.start();
+        e.drive();
+        e.charge();
+    }
+}
