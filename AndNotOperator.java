4a44f46080a6e193481bba2fead96410cbd9639e
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-03-11 22:14:43 +0530

    11 March commit

--- AndNotOperator.java
+++ AndNotOperator.java
@@ -6,6 +6,7 @@ public class AndNotOperator {
         boolean isSunny =true;
         int temp = 30;
         if(temp>15 && temp<40 || !isSunny){
+            System.out.println("Weather is good!");
             System.out.println("It's a cloudy weather");
         }
         else{
