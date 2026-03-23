6ab3e25db6da5a3694987dbe8f1c3d714cbf55ac
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-03-23 22:21:18 +0530

    Abstract Class with Static Method

---
+++ AbstractStatic.java
@@ -0,0 +1,10 @@
+abstract class Utility{
+    static void print(){
+        System.out.println("Hello from utility class!");
+    }
+}
+public class AbstractStatic {
+    public static void main(String[] args) {
+        Utility.print();
+    }
+}
