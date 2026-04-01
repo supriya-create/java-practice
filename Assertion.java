709420612b3bd4f76d5d5740a6595ad114eddae7
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-04-01 22:23:57 +0530

    Basic Assertion

---
+++ Assertion.java
@@ -0,0 +1,7 @@
+public class Assertion {
+    public static void main(String[] args) {
+        int age = 20;
+        assert age>=18 : "Age must be 18+";
+        System.out.println("Valid age"); 
+    }
+}
